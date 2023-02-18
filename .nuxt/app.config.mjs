1099f863692b4926386eb74886887ccbbf307215
TM <tm@oxenfree.net>
2023-02-18 18:57:53 +0700

    initial commit

--- .nuxt/app.config.mjs
+++
@@ -1,8 +0,0 @@
-
-import { defuFn } from '/home/tm/Documents/web/mod/node_modules/defu/dist/defu.mjs'
-
-const inlineConfig = {}
-
-
-
-export default defuFn(inlineConfig)
--- .nuxt/dist/server/client.manifest.mjs
+++
@@ -1,1 +0,0 @@
-export { default } from "file:///home/tm/Documents/web/mod/node_modules/@nuxt/vite-builder/dist/runtime/client.manifest.mjs"
