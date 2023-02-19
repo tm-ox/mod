5ea7d61bbfaf09a77cc8fafdd380c22662840171
TM <tm@oxenfree.net>
2023-02-19 12:50:46 +0700

    head

---
+++ components/Head.vue
@@ -0,0 +1,30 @@
+<template>
+  <Head>
+    <!-- Primary Meta Tags -->
+    <Title>Mrs. O'Donoghue</Title>
+    <Meta name="title" content="Mrs. O'Donoghue" />
+    <Meta name="description" content="Mrs. O'Donoghue - Home Made Foods" />
+    <!-- Open Graph / Facebook -->
+    <Meta property="og:type" content="website" />
+    <Meta property="og:url" content="https://mrsodonoghue.netlify.app" />
+    <Meta property="og:title" content="Mrs. O'Donoghue" />
+    <Meta
+      property="og:description"
+      content="Mrs. O'Donoghue - Home Made Foods"
+    />
+    <Meta property="og:image" content="images/ModBadge/png" />
+    <!-- Twitter -->
+    <Meta property="twitter:card" content="summary_large_image" />
+    <Meta property="twitter:url" content="https://mrsodonoghue.netlify.app" />
+    <Meta property="twitter:title" content="Mrs. O'Donoghue" />
+    <Meta
+      property="twitter:description"
+      content="Mrs. O'Donoghue - Home Made Foods"
+    />
+    <Meta property="twitter:image" content="images/ModBadge/png" />
+
+    <Link rel="icon" type="image/png" href="favicon.png" />
+    <Meta name="description" content="Mrs. O'Donoghue" />
+    <Script src="https://identity.netlify.com/v1/netlify-identity-widget.js" />
+  </Head>
+</template>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,27 +1,8 @@
 <template>
   <div>
+    <Head />
     <Header />
     <slot />
     <Footer />
   </div>
 </template>
-
-<script lang="ts">
-export default {
-  head() {
-    return {
-      title: "Mrs O'Donoghue",
-      link: [
-        {
-          rel: "icon",
-          type: "image/png",
-          href: `favicon.png`,
-        },
-      ],
-      script: [
-        { src: "https://identity.netlify.com/v1/netlify-identity-widget.js" },
-      ],
-    };
-  },
-};
-</script>
