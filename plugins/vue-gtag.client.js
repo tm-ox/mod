c8ab771c21b1907f3cabeb48b506ac1e79df2725
TM <tm@oxenfree.net>
2023-02-20 20:20:03 +0700

    gtag

--- plugins/vue-gtag.client.js
+++ plugins/vue-gtag.client.js
@@ -1,9 +1,23 @@
-import VueGtag from "vue-gtag-next";
+// import VueGtag from "vue-gtag-next";
+
+// export default defineNuxtPlugin((nuxtApp) => {
+//   nuxtApp.vueApp.use(VueGtag, {
+//     property: {
+//       id: "G-0MY9T6RBZX",
+//     },
+//   });
+// });
+
+import VueGtag from "vue-gtag";
 
 export default defineNuxtPlugin((nuxtApp) => {
-  nuxtApp.vueApp.use(VueGtag, {
-    property: {
-      id: "G-0MY9T6RBZX",
+  nuxtApp.vueApp.use(
+    VueGtag,
+    {
+      config: {
+        id: "G-0MY9T6RBZX",
+      },
     },
-  });
+    nuxtApp.$router
+  );
 });
