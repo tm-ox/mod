212ffd8707cf8caee65c9525acd705706ff0bf31
TM <tm@oxenfree.net>
2023-02-20 20:51:13 +0700

    gtag

--- plugins/vue-gtag.client.js
+++ plugins/vue-gtag.client.js
@@ -1,13 +1,3 @@
-// import VueGtag from "vue-gtag-next";
-
-// export default defineNuxtPlugin((nuxtApp) => {
-//   nuxtApp.vueApp.use(VueGtag, {
-//     property: {
-//       id: "G-0MY9T6RBZX",
-//     },
-//   });
-// });
-
 import VueGtag from "vue-gtag";
 
 export default defineNuxtPlugin((nuxtApp) => {
