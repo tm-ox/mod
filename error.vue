02530fdb42ac25a2cf1733e5908db202433c9f62
TM <tm@oxenfree.net>
2023-02-20 16:59:34 +0700

    head, dropshadow, tweaks, error

---
+++ error.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <Header />
+    <main>
+      <section>
+        <h2 v-if="error.statusCode === 404">404 - Page Not Found</h2>
+        <h2 v-else>Page Not Found</h2>
+      </section>
+    </main>
+    <Footer />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    error: {
+      type: Object,
+      default: null,
+    },
+  },
+};
+</script>
